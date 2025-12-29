db8190d972b7a3070457aed32f7a67ce2d2d9833
Sandeep Morchhale <sandeepmorchhale2002@gmail.com>
2025-12-29 17:22:52 +0530

    its a last fix in server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-
 const express = require("express")
 const app = express()
+
+app.set("trust proxy", 1); 
+
 const cookieParser = require("cookie-parser")
 const authroute = require("./routes/auth.routes")
 const userroute = require("./routes/user.routes")
@@ -10,23 +12,21 @@ const cors = require("cors")
 
 app.use(cors({
    origin: ["http://localhost:5173", "https://sansonmart-frontend.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+   methods: ["GET", "POST", "PUT", "DELETE"],
+   credentials: true
 }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+
 app.use("/api/auth", authroute)
 app.use("/api/user", userroute)
 app.use("/api/admin", adminrouter)
 app.use("/uploads", express.static("uploads"));
 
-
-
 app.get("/", (req, res) => {
     res.send("hellow i am comming ")
 })
 
-
-module.exports = app 
+module.exports = app
